c88a31b56959085c6da8afc4581f25fa4950b0c9
Hafiz Rahim Adnan <hafizrahim3011@gmail.com>
2025-07-28 22:04:56 +0500

    complete controllers for delete add update and getting doctors

--- server/src/controllers/doctor.controller.js
+++ server/src/controllers/doctor.controller.js
@@ -162,7 +162,7 @@ export const logoutDoctor = wrapAsync(async (req, res) => {
     .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, "User Logged out successfully", loggedOutUser));
 });
-
+// ! FIX:also delete user on deleting the doctor and same in delete all doctors controller
 export const deleteDoctor = wrapAsync(async (req, res) => {
   const { doctorId } = req.params;
   if (!doctorId) {
@@ -186,6 +186,137 @@ export const deleteDoctor = wrapAsync(async (req, res) => {
     new ApiResponse(200, "Doctor deleted successfully", {
       deletedUser: deletedUser,
       deletedDoctor: deletedDoctor,
+    })
+  );
+});
+
+export const getOneDoctor = wrapAsync(async (req, res) => {
+  const user = req.user;
+  const { doctorId } = req.params;
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const doctor = await Doctor.findOne({ user: doctorId });
+  if (!doctor) {
+    throw new ApiError(404, "Doctor not found of this Id");
+  }
+
+  res.status(200).json(
+    new ApiResponse(200, "Doctor Shown successfully", {
+      user: user,
+      doctor: doctor,
+    })
+  );
+});
+
+export const getAllDoctors = wrapAsync(async (req, res) => {
+  const allDoctors = await Doctor.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "userInfo",
+      },
+    },
+    {
+      $unwind: "$userInfo",
+    },
+  ]);
+  if (!allDoctors) {
+    throw new ApiError(404, "No doctors found in Database");
+  }
+  res.status(200).json(
+    new ApiResponse(200, "All doctors shown successfully ", {
+      doctors: allDoctors,
+    })
+  );
+});
+
+export const deleteAllDoctors = wrapAsync(async (req, res) => {
+  const allDeletedDoctors = await Doctor.deleteMany({});
+  if (deleteAllDoctors < 0) {
+    throw new ApiError(401, "No doctors found to delete");
+  }
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        "All doctors deleted successfully",
+        allDeletedDoctors
+      )
+    );
+});
+
+export const updateDoctor = wrapAsync(async (req, res) => {
+  let userId = req.user._id;
+  let { doctorId } = req.params;
+  if (!doctorId) {
+    throw new ApiError(404, "Doctor of this id not found");
+  }
+
+  let {
+    username,
+    email,
+    password,
+    Specialty,
+    experience,
+    Fee,
+    Address,
+    Education,
+    About,
+  } = req.body;
+
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "No user found of this Id");
+  }
+
+  if (username) user.username = username;
+  if (password) user.password = password;
+  if (email) user.email = email;
+
+  let updatedUser = await user.save({ validateBeforeSave: false });
+
+  if (!updatedUser) {
+    throw new ApiError(500, "Failed to update a User");
+  }
+  let doctor = await Doctor.findOne({ user: doctorId });
+
+  let profilePicPath = req.files?.profilePic[0]?.path;
+  if (!profilePicPath) {
+    throw new ApiError(401, "Path for Profile Path is required");
+  }
+
+  let profilePic = await uploadOnCloudinary(profilePicPath);
+  if (!profilePic) {
+    throw new ApiError(500, "Failed to upload an image");
+  }
+
+  if (!doctor) {
+    throw new ApiError(404, "No Doctor found of this Id");
+  }
+
+  if (Specialty) doctor.Specialty = Specialty;
+  if (experience) doctor.experience = experience;
+  if (Fee) doctor.Fee = Fee;
+  if (Address) doctor.Address = Address;
+  if (Education) doctor.Education = Education;
+  if (About) doctor.About = About;
+  if (profilePic) doctor.profilePic = profilePic.url;
+
+  const updatedDoctor = await doctor.save();
+
+  if (!updatedDoctor) {
+    throw new ApiError(500, "Failed to update A Doctor");
+  }
+
+  res.status(200).json(
+    new ApiResponse(200, "Doctor Updated successfully", {
+      updatedUser: updatedUser,
+      updatedDoctor: updatedDoctor,
     })
   );
 });
--- server/src/routes/doctor.route.js
+++ server/src/routes/doctor.route.js
@@ -1,8 +1,32 @@
 import Router from "express";
-import { addDoctor, deleteDoctor } from "../controllers/doctor.controller.js";
+import {
+  addDoctor,
+  deleteAllDoctors,
+  deleteDoctor,
+  getAllDoctors,
+  getOneDoctor,
+  updateDoctor,
+} from "../controllers/doctor.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
+router
+  .route("/doctor")
+  .get(verifyJwt("admin"), getAllDoctors)
+  .delete(deleteAllDoctors);
+router
+  .route("/doctor/:doctorId")
+  .get(verifyJwt("doctor" || "admin"), getOneDoctor)
+  .put(
+    verifyJwt("admin"),
+    upload.fields([
+      {
+        name: "profilePic",
+        maxCount: 1,
+      },
+    ]),
+    updateDoctor
+  );
 router.route("/doctor/add").post(
   verifyJwt("admin"),
   upload.fields([
